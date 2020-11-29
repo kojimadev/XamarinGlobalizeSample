b71fa348d44e3dfa7863b57e8922c33242dd2b48
kojimadev <kojimadev@gmail.com>
2020-11-29 23:51:29 +0900

    Xamarin.Formsのサンプルプロジェクトを作成

---
+++ src/XamarinGlobalizeSample/XamarinGlobalizeSample/TranslateExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinGlobalizeSample.Resources;
+
+namespace XamarinGlobalizeSample
+{
+	/// <summary>
+	/// TranslateのXAMLマークアップ拡張により、リソース文字列を取得する
+	/// </summary>
+	[ContentProperty("Key")]  // Keyを規定のプロパティに指定 
+	public class TranslateExtension : IMarkupExtension
+	{
+		/// <summary>
+		/// リソース文字列のプロパティ名として利用する文字列
+		/// </summary>
+		public string Key { get; set; }
+
+		/// <summary>
+		/// IMarkupExtension インタフェースとしてのマークアップ拡張処理
+		/// </summary>
+		/// <param name="serviceProvider"></param>
+		/// <returns></returns>
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (string.IsNullOrEmpty(Key))
+			{
+				return "Key is null";
+			}
+
+			// Keyプロパティに指定されたキーをもとにリソースから文字列を取得
+			ResourceManager resourceManager = AppResources.ResourceManager;
+			return resourceManager.GetString(Key, AppResources.Culture) ?? Key;
+		}
+	}
+}
